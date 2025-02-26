9271df00bf1e640ae73cb50f8d91e541ce6a7415
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-26 21:16:35 +1100

    better completions

--- completions/cse.fish
+++ completions/cse.fish
@@ -1,10 +1,11 @@
-# Disable file completions
-complete -c cse -f
+function __fish_cse_no_args
+    set -l cmd_len (commandline -cp | string split -n ' ' | count)
+    test $cmd_len = 1 -o \( $cmd_len = 2 -a (commandline -cp | string sub -s -1) != ' ' \)
+end
 
-# Subcommands
-set -l not_seen "not __fish_seen_subcommand_from help local remote pull push"
-complete -c cse -n $not_seen -a 'help' -d 'Show help'
-complete -c cse -n $not_seen -a 'local' -d 'Print local directory'
-complete -c cse -n $not_seen -a 'remote' -d 'Print remote directory'
-complete -c cse -n $not_seen -a 'pull' -d 'Pull changes from remote to local'
-complete -c cse -n $not_seen -a 'push' -d 'Push changes from local to remote'
+set -l not_seen "__fish_cse_no_args"
+complete -c cse -n $not_seen -a 'help' -d 'Show help' -f
+complete -c cse -n $not_seen -a 'local' -d 'Print local directory' -f
+complete -c cse -n $not_seen -a 'remote' -d 'Print remote directory' -f
+complete -c cse -n $not_seen -a 'pull' -d 'Pull changes from remote to local' -f
+complete -c cse -n $not_seen -a 'push' -d 'Push changes from local to remote' -f
