38aa40f940dad27e561d8deb78d859cdb7f8e7f9
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-19 17:17:52 +1100

    Create test function
    Provides an easy way to autotest assignments against custom test cases

--- completions/cse.fish
+++ completions/cse.fish
@@ -3,9 +3,10 @@ function __fish_cse_no_args
     test $cmd_len = 1 -o \( $cmd_len = 2 -a (commandline -cp | string sub -s -1) != ' ' \)
 end
 
-set -l not_seen "__fish_cse_no_args"
-complete -c cse -n $not_seen -a 'help' -d 'Show help' -f
-complete -c cse -n $not_seen -a 'local' -d 'Print local directory' -f
-complete -c cse -n $not_seen -a 'remote' -d 'Print remote directory' -f
-complete -c cse -n $not_seen -a 'pull' -d 'Pull changes from remote to local' -f
-complete -c cse -n $not_seen -a 'push' -d 'Push changes from local to remote' -f
+set -l no_args "__fish_cse_no_args"
+complete -c cse -n $no_args -a 'help' -d 'Show help' -f
+complete -c cse -n $no_args -a 'local' -d 'Print local directory' -f
+complete -c cse -n $no_args -a 'remote' -d 'Print remote directory' -f
+complete -c cse -n $no_args -a 'pull' -d 'Pull changes from remote to local' -f
+complete -c cse -n $no_args -a 'push' -d 'Push changes from local to remote' -f
+complete -c cse -n $no_args -a 'test' -d 'Autotest assignments against self-made test cases' -f
--- install.fish
+++ install.fish
@@ -1,10 +1,10 @@
 #!/bin/fish
 
 set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME || set -l config $HOME/.config
-set -l src (dirname (status filename))
+set -l src (dirname (realpath (status filename)))
 
 mkdir -p $HOME/.local/bin
 mkdir -p $config/fish/completions
 
-cp $src/cse $HOME/.local/bin/cse
+ln -sf $src/cse $HOME/.local/bin/cse
 cp $src/completions/cse.fish $config/fish/completions/cse.fish
