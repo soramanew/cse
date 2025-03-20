5259ed191245138265d6f00cd2fedb080893cfe4
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-20 13:50:27 +1100

    update command
    Also more verbose

--- completions/cse.fish
+++ completions/cse.fish
@@ -5,6 +5,7 @@ end
 
 set -l no_args "__fish_cse_no_args"
 complete -c cse -n $no_args -a 'help' -d 'Show help' -f
+complete -c cse -n $no_args -a 'update' -d 'Update this script' -f
 complete -c cse -n $no_args -a 'local' -d 'Print local directory' -f
 complete -c cse -n $no_args -a 'remote' -d 'Print remote directory' -f
 complete -c cse -n $no_args -a 'pull' -d 'Pull changes from remote to local' -f
