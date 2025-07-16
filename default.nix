801d6201175f7bbd3bfe57aecaf797bb82a061b2
Soramane <61896496+soramanew@users.noreply.github.com>
2025-07-16 15:45:27 +1000

    nix: some fixes
    patch stuff fix deps

--- default.nix
+++ default.nix
@@ -2,24 +2,42 @@
   rev,
   lib,
   stdenvNoCC,
+  makeWrapper,
   installShellFiles,
   fish,
-}:
-stdenvNoCC.mkDerivation {
-  pname = "cse";
-  version = "${rev}";
-  src = ./.;
+  openssh,
+  rsync,
+  coreutils,
+  gcc,
+}: let
+  runtimeDeps = [fish openssh rsync coreutils gcc];
+in
+  stdenvNoCC.mkDerivation {
+    pname = "cse";
+    version = "${rev}";
+    src = ./.;
 
-  nativeBuildInputs = [installShellFiles];
-  buildInputs = [fish];
+    nativeBuildInputs = [makeWrapper installShellFiles];
+    propagatedBuildInputs = runtimeDeps;
 
-  installPhase = "install -Dm755 cse $out/bin/cse";
-  postInstall = "installShellCompletion completions/cse.fish";
+    patchPhase = ''
+      substituteInPlace cse \
+        --replace-fail '$src/tests' "$src/tests" \
+        --replace-fail "echo 'Updating...'" "echo 'Self updates are not available on NixOS.'; exit 1" \
+        --replace-fail 'dcc --valgrind --leak-check' 'gcc'
+    '';
 
-  meta = {
-    description = "A helper script for UNSW CSE students";
-    homepage = "https://github.com/soramanew/cse";
-    mainProgram = "cse";
-    platforms = lib.platforms.linux;
-  };
-}
+    installPhase = ''
+      install -Dm755 cse $out/bin/cse
+      wrapProgram $out/bin/cse --prefix PATH : ${lib.makeBinPath runtimeDeps}
+    '';
+
+    postInstall = "installShellCompletion completions/cse.fish";
+
+    meta = {
+      description = "A helper script for UNSW CSE students";
+      homepage = "https://github.com/soramanew/cse";
+      mainProgram = "cse";
+      platforms = lib.platforms.linux;
+    };
+  }
